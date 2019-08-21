165d45384d6fa209f63e5c34ee36475c2873c641
麋鹿 <1669277128@qq.com>
2019-08-21 20:01:40 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+   <!-- <router-view></router-view> -->
+   <p>这是根组件</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+
+}
+</script>
+
+<style lang="less">
+
+</style>
