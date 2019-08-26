22838d93b1b7a103a517e576088f6da7668c0591
麋鹿 <1669277128@qq.com>
2019-08-26 17:07:34 +0800

    实现权限列表和角色列表的组件创建和路由配置

---
+++ src/api/right_index.js
@@ -0,0 +1,9 @@
+// 1.引入封装好的axios
+// import axios from '@/utils/myaxios.js'
+
+// 2.实现权限列表数据获取
+// export const getAllRight = () => {
+//   return axios({
+
+//   })
+// }
---
+++ src/views/rights/rightList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="rightList">
+      <span>权限列表</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
