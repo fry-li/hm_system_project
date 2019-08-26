c66a8a0af1a28bba68dc5e6fc6aec32c70231ab0
麋鹿 <1669277128@qq.com>
2019-08-26 18:45:15 +0800

    完实现权限列表的页面结构创建

--- src/api/right_index.js
+++ src/api/right_index.js
@@ -1,9 +1,9 @@
 // 1.引入封装好的axios
-// import axios from '@/utils/myaxios.js'
+import axios from '@/utils/myaxios.js'
 
 // 2.实现权限列表数据获取
-// export const getAllRight = () => {
-//   return axios({
-
-//   })
-// }
+export const getAllRight = (type) => {
+  return axios({
+    url: `rights/${type}`
+  })
+}
